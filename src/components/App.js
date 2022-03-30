bb5915fa3593a4ccd78da00a6abf472799be01e8
Shruthi <shruthirapala99@gmail.com>
2022-03-30 17:52:05 -0400

    Added header and add contact form

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import React from 'react';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+
 
 function App() {
   return (
-    <div>Hello React Application</div>
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      {/* <ContactList /> */}
+    </div>
   );
 }
 
