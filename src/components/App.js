0f4699cc7e08d7fc25b950052c777b79f8633ee3
Shruthi <shruthirapala99@gmail.com>
2022-03-31 18:02:42 -0400

    Added Delete functionality to contact list

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import { v4 as uuid } from 'uuid';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
@@ -11,9 +12,17 @@ const [contacts, setContacts] = useState([]);
 const addContactHandler = (contact) => {
   // to get data from child component to parent component we use handlers 
   // to add the contact to the list use setcontact to update the state and add to the exsisting list
-  setContacts([...contacts, contact]);
+  setContacts([...contacts, { id: uuid(), ...contact }]);
  
 };
+
+ const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
 
  useEffect(() => {
     const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
@@ -25,15 +34,11 @@ useEffect(() => {
   // add to local storage
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
 }, [contacts])
-
-
-
-
   return (
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler}/>
-      <ContactList contacts = {contacts}/>
+      <ContactList contacts = {contacts} getContactId={removeContactHandler}/>
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,12 +3,15 @@ import ContactCard from './ContactCard'
 
 const ContactList = (props) => {
     console.log(props);
+    const deleteConactHandler = (id) => {
+    props.getContactId(id);
+    };
     const renderContactList = props.contacts.map((contact) => {
        return (
-           <ContactCard contact={contact}/>
+           <ContactCard contact={contact} 
+        clickHander={deleteConactHandler}
+        key={contact.id}/>
        )
-           
-       
     })
     return(
         
