dcb98daa016d2770b02319a08a0ce9e92a46bd1d
Shruthi <shruthirapala99@gmail.com>
2022-03-31 15:17:58 -0400

    Added local storage using useEffect React hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,38 @@
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 
 function App() {
+const LOCAL_STORAGE_KEY = "contacts"
+const [contacts, setContacts] = useState([]);
+const addContactHandler = (contact) => {
+  // to get data from child component to parent component we use handlers 
+  // to add the contact to the list use setcontact to update the state and add to the exsisting list
+  setContacts([...contacts, contact]);
+ 
+};
 
-  const contacts = [
-    { 
-      id: "1", 
-      name: "John",
-      email: "john@example.com"
-    },
-    { 
-      id: "2", 
-      name:"Smith",
-      email: "smith@example.com"
-    },
-    { 
-    id:"3",
-    name:"Kevin",
-    email: "kevin@example.com"
-    }
-  ]
+ useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+// to persist data, when ever the value changes it helps to render the component again
+useEffect(() => {
+  // add to local storage
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+}, [contacts])
+
+
+
+
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts = {contacts}/>
     </div>
   );
