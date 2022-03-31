374f1dd22ba819aa7a542bd9202dce921fd24071
Shruthi <shruthirapala99@gmail.com>
2022-03-31 12:29:52 -0400

    Create and render contact list

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,29 @@ import ContactList from './ContactList';
 
 
 function App() {
+
+  const contacts = [
+    { 
+      id: "1", 
+      name: "John",
+      email: "john@example.com"
+    },
+    { 
+      id: "2", 
+      name:"Smith",
+      email: "smith@example.com"
+    },
+    { 
+    id:"3",
+    name:"Kevin",
+    email: "kevin@example.com"
+    }
+  ]
   return (
     <div className="ui container">
       <Header />
       <AddContact />
-      {/* <ContactList /> */}
+      <ContactList contacts = {contacts}/>
     </div>
   );
 }
